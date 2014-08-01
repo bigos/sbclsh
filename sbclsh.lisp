854fc58fbc8984b9d3d2dc322bdd0a1c9ede319a
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2014-08-01 16:21:11 +0100

    more experiments with types

--- sbclsh.lisp
+++ sbclsh.lisp
@@ -12,6 +12,25 @@
 ;;; types in sbcl
 (defparameter x 14)
 (proclaim '(type (integer 0 15) x) )
-(setq x 16)                             ; should raise an error
+
+;;; this should not compile
+;;; (setq x 16)
+
 ;;; more info about types
 ;;; http://www.sbcl.org/manual/#Getting-Existing-Programs-to-Run
+
+(defun print-number (x)
+  (declare (type (integer 0 15) x))
+  (format t "~a~%" x))
+
+(defun print-string (str)
+  (declare (type string str))
+  (format t "~A~%" str))
+
+(defun print-number-and-strings (num str1 str2)
+  (declare (type fixnum num)
+           (type string str1 str2))
+  (format t "~A~A~A" num str1 str2))
+
+;;; this won't compile because of type mismatch
+;;; (print-string 1)

f727540bb30da5884994db7eb316f5b94c55c989
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2014-08-01 15:37:05 +0100

    experimenting with types

--- sbclsh.lisp
+++ sbclsh.lisp
@@ -6,4 +6,10 @@
 
 (defun main (arg)
   (let ((argv sb-ext:*posix-argv* ))
-    (format T "Hello ~A ~A~%" argv arg)))
+    (format nil "Hello ~A ~A~%" argv arg)))
+
+
+;;; types in sbcl
+(defparameter x 14)
+(proclaim '(type (integer 0 15) x) )
+(setq x 16)                             ; should raise an error
